1f1c86664684e8d1fcb6bbd02b6b7cffa15e4ccc
mugisha Samuel <mugishasamuel42@gmail.com>
2022-02-07 23:13:06 -0800

    Fix pure function problem in class component definition

---
+++ src/components/calculator.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = '';
+  }
+
+  render() {
+    return (
+      <section>
+        <div />
+        <div>
+          <div>
+            <div />
+            <div />
+          </div>
+          <div />
+        </div>
+
+      </section>
+    );
+  }
+}
+
+export default Calculator;
